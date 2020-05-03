15cbf83625e9a61646169611fae0c4d6a02a32f1
armaan thapa <armaanthapa724@gmail.com>
2020-05-03 14:24:17 +0530

    Adding Logger in project

--- src/main/java/com/xworkz/main/controller/LoggController.java
+++ src/main/java/com/xworkz/main/controller/LoggController.java
@@ -1,5 +1,6 @@
 package com.xworkz.main.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +10,28 @@ public class LoggController {
 	
 	
 	
+	private static final Logger looger= Logger.getLogger(LoggController.class);
+	
+	
 	public LoggController() {
-System.err.println("created   "  +this.getClass().getSimpleName());	}
+		looger.warn("Created   "  +this.getClass().getSimpleName());
+	}
 	
 	@RequestMapping(value="/logg")
 	public String loggController()
 	{
+		System.out.println("armaan thapa");
+		
+		looger.info("logging info example");
+		int i=8;
+		
+		
+		try {
+			int s=i/0;
+			
+		} catch (Exception e) {
+			looger.error(e.getMessage(),e);
+		}
 		
 		return "Logg";
 	}
